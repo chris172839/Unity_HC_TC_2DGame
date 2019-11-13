39da4777b59458bd23a04ea7db148b726f11ea57
chris172839 <57192859+chris172839@users.noreply.github.com>
2019-11-13 21:37:57 +0800

    1113

--- UnityProject/Assets/Scripts/Floor.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public class Floor : MonoBehaviour
-{
-    [Header("地板移動速度"),Range(0.1f,100f)]
-    public float speed = 1f;
-}
---
+++ UnityProject/Assets/Scripts/Ground.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    public Transform ground;
+
+    [Header("地板移動速度"),Range(0,100f)]
+    public float speed = 1f;
+    void Update()
+    {
+        // Move the object forward along its z axis 1 unit/second.
+        ground.Translate(-speed, 0, 0);
+    }
+}
